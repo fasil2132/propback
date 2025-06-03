55998c695838dad4ed7a9d26952a5b3e0c08a406
fasilz <fasilzegeye@gmail.com>
2025-06-03 14:04:06 +0300

    Update dashboard.routes.ts

--- src/routes/dashboard.routes.ts
+++ src/routes/dashboard.routes.ts
@@ -1,4 +1,4 @@
-import { express } from "express";
+import express from "express";
 import { getDashboardData, getMyProperties } from "../controllers/property.controller";
 import { authenticate } from "../middleware/auth.middleware";
 
