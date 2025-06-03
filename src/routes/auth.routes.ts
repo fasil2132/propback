4a89c1b7e756506f11f90c69f1cec70534ba1d1c
fasilz <fasilzegeye@gmail.com>
2025-06-03 13:41:02 +0300

    Update auth.routes.ts

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,9 +1,9 @@
-import { Router } from 'express-serve-static-core';
+import { express } from 'express';
 import { login, register, validateToken } from '../controllers/auth.controller';
 // import { validate } from 'express-validation';
 // You might want to add validation middleware here
 
-const router = Router();
+const router = express.Router();
 
 router.post('/login', login);
 router.post('/register', register);
