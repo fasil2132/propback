b04fba48079dead1113918f25d7ff5482d7d9614
fasilz <fasilzegeye@gmail.com>
2025-06-03 12:18:52 +0300

    Update owner.controller.ts

--- src/controllers/owner.controller.ts
+++ src/controllers/owner.controller.ts
@@ -1,4 +1,5 @@
-import { Request, Response, NextFunction } from "express";
+// import { Request, Response, NextFunction } from "express";
+import { Request, Response, NextFunction } from 'express-serve-static-core';
 import ApiError from "../utils/apiError";
 
 import { User } from "../types/user";
