3a1ee03ee14392a53256dac675d3406c9d6e6a79
fasilz <fasilzegeye@gmail.com>
2025-06-03 13:41:19 +0300

    Update dashboard.routes.ts

--- src/routes/dashboard.routes.ts
+++ src/routes/dashboard.routes.ts
@@ -1,8 +1,8 @@
-import { Router } from "express-serve-static-core";
+import { express } from "express";
 import { getDashboardData, getMyProperties } from "../controllers/property.controller";
 import { authenticate } from "../middleware/auth.middleware";
 
-const router = Router();
+const router = express.Router();
 
 router.get("/:userId", authenticate, getDashboardData);
 
