68841580d0fe0d1c20efa7d5a612895e5ce67083
fasilz <fasilzegeye@gmail.com>
2025-06-03 12:24:14 +0300

    Update owner.routes.ts

--- src/routes/owner.routes.ts
+++ src/routes/owner.routes.ts
@@ -1,4 +1,4 @@
-import { Router } from 'express';
+import { Router } from 'express-serve-static-core';
 import { getOwnerDashboard, getAllOwners } from '../controllers/owner.controller';
 import { authenticate } from '../middleware/auth.middleware';
 
